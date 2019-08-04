cbc26e62a13001eab4cbcde1f62257c77c0546c1
Jamal <jamalabdul47@gmail.com>
2019-08-04 10:49:37 -0400

    calculator finished

--- js/index.js
+++ js/index.js
@@ -18,8 +18,31 @@ function printTime() {
   document.getElementById('container__clock').innerHTML = clockString;
 }
 
+const btn = document.getElementById('container__btn');
 
+function calc() {
+  const value1 = parseInt(document.getElementById('value1').value);
+  const value2 = parseInt(document.getElementById('value2').value);
+  const selector = document.getElementById('selector').value;
+  let res;
+
+  if(selector === 'add'){
+    res = value1 + value2;
+  }else if (selector === 'sub'){
+    res = value1 - value2;
+  }else if (selector === 'maul'){
+    res = value1 * value2;
+  }else if (selector === 'div'){
+    res = value1 / value2;
+  }
+
+  alert(res);
+
+}
 
 
 
 setInterval(printTime, 0);
+btn.addEventListener('click', ()=>{
+  calc();
+});
